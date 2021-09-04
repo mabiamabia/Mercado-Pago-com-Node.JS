63d49275259f63d6bc33893bd14b43f9acb2f092
Priscila Duarte <duartecostap@gmail.com>
2021-09-03 21:07:09 -0300

    Node é massa

--- BootstrapProjects/NodeJS/app.js
+++ BootstrapProjects/NodeJS/app.js
@@ -1,4 +1,4 @@
-var mostrarSite = true;
+/* var mostrarSite = true;
 const site = "www.google.com"
 
 console.log("Hello world!");
@@ -6,4 +6,21 @@ console.log("Olá Mundo!");
 
 if(mostrarSite){
     console.log(site)
-}
+} */
+
+//importar função soma
+var calculadora = require("./calculadora");
+console.log(calculadora.soma(10,10));
+console.log(calculadora.mult(10,20));
+console.log(calculadora.sub(40,5));
+console.log(calculadora.nome);
+
+
+
+/* var resultado = mult(10,10);
+var resultadoSub = sub(20,40);
+
+console.log(soma(10,25));
+console.log(div(10,2));
+console.log(resultado);
+console.log(resultadoSub); */
---
+++ BootstrapProjects/NodeJS/calculadora.js
@@ -0,0 +1,27 @@
+var nome = "Minha calculadora V1"
+
+function soma(a, b) {
+    return a + b;
+}
+
+function mult(a,b){
+    return a* b;
+}
+
+function sub(a,b){
+    return a - b;
+}
+
+function div(a,b){
+    return a / b;
+}
+
+module.exports = {
+    soma, 
+    mult,
+    sub,
+    div,
+    nome
+}
+
+
